4eedcbcaa0e85fa34d1204d805ecd6399ef08a8b
smartdev425 <anthon.dev914@gmail.com>
2021-07-27 17:35:04 -0400

    Added jest setup

---
+++ src/components/loader/PageLoader.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View } from 'react-native';
+import { render } from '@testing-library/react-native';
+
+import { PageLoader } from './PageLoader.js';
+
+describe('PageLoader', function() {
+  let screen;
+
+  beforeEach(() => {
+    screen = render(
+      <View testID="Root">
+        <PageLoader
+          testID="PageLoader"
+        />
+      </View>
+    );
+  });
+
+  it('should render', () => {
+    expect(screen.getByTestId('Root'))
+      .toContainElement(screen.getByTestId('PageLoader'));
+  });
+});
