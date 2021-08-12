2f5177a319b4272e40a189d7312ccb6a2aad7de9
smartdev425 <anthon.dev914@gmail.com>
2021-08-11 18:33:52 -0600

    Resolved an issue where path aliases didn't work in storybook native.

--- storybook/babel.config.js
+++ storybook/babel.config.js
@@ -2,5 +2,16 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['module:react-native-dotenv', {
+        safe: true,
+        allowUndefined: true,
+      }],
+      ['module-resolver', {
+        'alias': {
+          '~': '../app/src',
+        }
+      }]
+    ]
   };
 };
