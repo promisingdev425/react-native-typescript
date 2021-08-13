c1b4190c2759d5a6a58c9e8021df6978cbaae25c
smartdev425 <anthon.dev914@gmail.com>
2021-08-13 13:20:21 -0700

    add logo image, tab icons

---
+++ app/src/assets/images/index.js
@@ -0,0 +1,23 @@
+/* ------------- Images ------------- */
+import logoImage from './images/logo-image.png'
+
+/* ------------- Icons ------------- */
+import tabBrowse from './icons/tab-browse.svg'
+import tabBrowseActive from './icons/tab-browse-active.svg'
+import tabLeaderboard from './icons/tab-leaderboard.svg'
+import tabtabLeaderboardActive from './icons/tab-leaderboard-active.svg'
+import tabSummary from './icons/tab-summary.svg'
+import tabSummaryActive from './icons/tab-summary-active.svg'
+
+
+export {
+  // Images
+  logoImage,
+  // Icons
+  tabBrowse,
+  tabBrowseActive,
+  tabLeaderboard,
+  tabtabLeaderboardActive,
+  tabSummary,
+  tabSummaryActive,
+}
