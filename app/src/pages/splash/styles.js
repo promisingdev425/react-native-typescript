f8b80d2958bdc947695d1aac71abfd077b6f5269
smartdev425 <anthon.dev914@gmail.com>
2021-08-13 19:14:39 -0700

    remove LinearGradient due to pod init

--- app/src/pages/splash/styles.js
+++ app/src/pages/splash/styles.js
@@ -1,18 +1,10 @@
 import styled from 'styled-components/native'
-import LinearGradient from 'react-native-linear-gradient'
 
-import { getColor } from '~/theme'
 import { Box } from '~/components'
 import { logoImage } from '~/assets/images'
 
-export const Container = styled(LinearGradient).attrs((props) => ({
-  colors: [
-    '#FFF', '#000'
-    // getColor('brandOrange')(props),
-    // getColor('brandPink')(props),
-  ],
-  start: { x: 0, y: 0 },
-  end: { x: 1, y: 1 },
+export const Container = styled(Box).attrs(() => ({
+  bg: 'brandOrange'
 }))`
   flex: 1;
   justify-content: center;
