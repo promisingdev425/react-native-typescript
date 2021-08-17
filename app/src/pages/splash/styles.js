934b636ab78aa0e36f896b7da73bbf52869ef233
smartdev425 <anthon.dev914@gmail.com>
2021-08-17 15:23:11 -0700

    add `LinearGradient` component, apply to splash screen

--- app/src/pages/splash/styles.js
+++ app/src/pages/splash/styles.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components/native'
+import { LinearGradient } from 'expo-linear-gradient'
 
-import { Box } from '~/components'
+import { getColor } from '~/theme'
 import { logoImage } from '~/assets/images'
 
-export const Container = styled(Box).attrs(() => ({
-  bg: 'brandOrange'
+export const Container = styled(LinearGradient).attrs((props) => ({
+  colors: [getColor('brandOrange')(props), getColor('brandPink')(props)],
+  start: [0.2, 0.2],
+  end: [0.8, 0.8],
 }))`
   flex: 1;
   justify-content: center;
