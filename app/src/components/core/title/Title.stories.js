26404a3ba7e043a5f9677d5a90c739caa620d92c
smartdev425 <anthon.dev914@gmail.com>
2021-08-18 22:35:25 -0500

    updated mdx by per usecases

--- app/src/components/core/title/Title.stories.js
+++ app/src/components/core/title/Title.stories.js
@@ -11,8 +11,8 @@ storiesOf('Title', module)
   .add('by variants', () => (
     <>
       <Title variant="navigation" pt={3}>This is [navigation] title</Title>
-      <Title variant="header" pt={3}>This is [header] title</Title>
-      <Title variant="section" pt={3}>This is [section] title</Title>
-      <Title variant="body" pt={3}>This is [body] title</Title>
+      <Title variant="header" color="positive" pt={3}>This is [header] title</Title>
+      <Title variant="section" color="warning" pt={3}>This is [section] title</Title>
+      <Title variant="body" color="negative" pt={3}>This is [body] title</Title>
     </>
   ));
--- storybook/babel.config.js
+++ storybook/babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', '@babel/preset-env', '@babel/preset-react'],
+    presets: ['babel-preset-expo'],
     plugins: [
       ['module:react-native-dotenv', {
         safe: true,
