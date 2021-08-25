24e5f6ec21c0e0761208bf44d79c44dadee542db
smartdev425 <anthon.dev914@gmail.com>
2021-08-25 14:20:13 -0700

    update box storybook description

--- app/src/components/core/box/Box.stories.js
+++ app/src/components/core/box/Box.stories.js
@@ -1,28 +1,21 @@
 import React from 'react'
 import { Text } from 'react-native'
-import styled from 'styled-components/native'
 import { storiesOf } from '@storybook/react-native'
 
 import { Centered } from '~/storybook-utils'
 
 import { Box } from './Box.jsx'
 
-const Container = styled(Box).attrs((props) => ({
-  bg: 'negative',
-  p: 6,
-}))``
-
 // Component Native Stories
 storiesOf('Box', module)
   .addDecorator((cb) => <Centered>{cb()}</Centered>)
-  .add('default', () => (
-    <Container>
-      <Box bg="brandPink" p={5} borderRadius={5}>
-        <Text>This is a brandPink Box</Text>
-      </Box>
-
-      <Box bg="brandOrange" p={5} borderRadius={5}>
-        <Text>This is a brandOrange Box</Text>
-      </Box>
-    </Container>
+  .add('Pink', () => (
+    <Box bg="brandPink" p={5} borderRadius={5}>
+      <Text>This is a brandPink Box</Text>
+    </Box>
+  ))
+  .add('Orange', () => (
+    <Box bg="brandOrange" p={5} borderRadius={5}>
+      <Text>This is a brandOrange Box</Text>
+    </Box>
   ))
