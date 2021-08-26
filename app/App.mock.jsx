2baa5cc2e5c7e6c212eaa1a88c45712e0d465c45
smartdev425 <anthon.dev914@gmail.com>
2021-08-26 15:13:57 -0700

    revert theming in app mock

--- app/App.mock.jsx
+++ app/App.mock.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { ThemeProvider } from 'styled-components/native'
 
 import { Auth0Mock } from '~/services/auth/mock'
-import { themes } from '~/theme'
-import useFonts from '~/assets/fonts/useFonts'
-
 import MainApp from './App.api'
+
 import { WithMocks } from './src/bootstrap/WithMocks.jsx'
 
 /**
@@ -24,25 +21,19 @@ export default function App({
   // Any other services or props will be passed through to <WithMocks>
   ...rest
 }) {
-  const theme = themes.light
-  const fontsLoaded = useFonts()
-
-  return fontsLoaded ? (
-    <ThemeProvider theme={theme}>
-      <MainApp
-        // Provide a mock implementation of our auth client.
-        authService={authService}
-      >
-        {/*
-          Provide a version of the app that uses mocks for
-          all services.
-        */}
-        <WithMocks {...rest} />
-      </MainApp>
-    </ThemeProvider>
-  ) : null
+  return (
+    <MainApp
+      // Provide a mock implementation of our auth client.
+      authService={authService}
+    >
+      {/*
+        Provide a version of the app that uses mocks for
+        all services.
+      */}
+      <WithMocks {...rest} />
+    </MainApp>
+  )
 }
-
 App.propTypes = {
   authService: PropTypes.object,
 }
