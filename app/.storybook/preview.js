822e853eaf5d1392c5af14bd21a6e7d7a1ded02a
smartdev425 <anthon.dev914@gmail.com>
2021-09-02 23:25:52 -0400

    add decorator ref link

--- app/.storybook/preview.js
+++ app/.storybook/preview.js
@@ -3,6 +3,8 @@ import { ThemeProvider } from "styled-components/native";
 
 import { themes } from '~/theme'
 
+// Define global decorators.
+// https://storybook.js.org/docs/react/writing-stories/decorators
 export const decorators = [
   (story) => (
     <ThemeProvider theme={themes.light}>{story()}</ThemeProvider>
