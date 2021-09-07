e4eae603b750b670492f6fa9905d56a242107778
smartdev425 <anthon.dev914@gmail.com>
2021-09-07 18:02:02 -0400

    add icon button stories

--- app/src/components/core/icon-button/IconButton.stories.js
+++ app/src/components/core/icon-button/IconButton.stories.js
@@ -2,10 +2,23 @@ import React from 'react'
 import { storiesOf } from '@storybook/react-native'
 
 import { Centered } from '~/storybook-utils'
+import { CircularArrow } from '~/assets/images'
 
 import { IconButton } from './IconButton'
 
 // Component Native Stories
 storiesOf('IconButton', module)
   .addDecorator((cb) => <Centered>{cb()}</Centered>)
-  .add('default', () => <IconButton />)
+  .add('Prev Arrow', () => (
+    <IconButton onPress={() => console.log('hit Circular arrow button')}>
+      <CircularArrow testID="IconButton" color="#333" />
+    </IconButton>
+  ))
+  .add('Next Arrow', () => (
+    <IconButton
+      onPress={() => console.log('hit Circular arrow button')}
+      style={{ transform: [{ rotate: '180deg' }] }}
+    >
+      <CircularArrow testID="IconButton" color="#567" />
+    </IconButton>
+  ))
