43b61393542f14037cc75374ccd82a00785fe00e
smartdev425 <anthon.dev914@gmail.com>
2021-10-01 05:52:20 -0700

    add chart library, define chart types, helpers

---
+++ app/src/components/chart/helpers.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { G, Circle, Line } from 'react-native-svg'
+
+import { IDecorator, IGrid } from './types'
+
+export const CircleDecorator = ({
+  radius = 4,
+  fill = 'white',
+  strokeColor = '#000',
+  strokeWidth = 2,
+  data,
+  x,
+  y,
+}: IDecorator) => {
+  return (
+    <>
+      {data.map((value, index) => (
+        <Circle
+          key={index}
+          cx={x(index)}
+          cy={y(value)}
+          r={radius}
+          stroke={strokeColor}
+          strokeWidth={strokeWidth}
+          fill={fill}
+        />
+      ))}
+    </>
+  )
+}
+
+export const Grid = ({
+  x,
+  y,
+  data,
+  ticks,
+  strokeColor,
+  activeIndex,
+  activeColor,
+}: IGrid) => (
+  <G>
+    {ticks.map((tick: number) => (
+      <Line
+        key={tick}
+        x1={10}
+        x2="100%"
+        y1={y(tick)}
+        y2={y(tick)}
+        stroke={strokeColor}
+        transform="translate(-5, 0)"
+      />
+    ))}
+    {data.map((value: number, index: number) => (
+      <Line
+        key={index}
+        y1={'0%'}
+        y2={'100%'}
+        x1={x(index)}
+        x2={x(index)}
+        transform="translate(0, 5)"
+        stroke={index === activeIndex ? activeColor : strokeColor}
+        strokeDasharray={
+          index === activeIndex || index === 0 || index === data.length - 1
+            ? 0
+            : 4
+        }
+      />
+    ))}
+  </G>
+)
+
+export const getMaxValue = (numbers: Array<number>) => {
+  const maxValue = Math.max(...numbers)
+  const digits = Math.floor(Math.log10(maxValue))
+  const units = Math.pow(10, digits)
+
+  return Math.ceil(maxValue / units) * units
+}
---
+++ app/src/components/chart/types.ts
@@ -0,0 +1,37 @@
+import { GridProps } from 'react-native-svg-charts'
+
+import { IBox } from '../core'
+
+interface ICommonProps {
+  strokeWidth?: number
+  strokeColor?: string
+}
+
+export type ChartData = {
+  value: number
+  label: string
+}
+
+export interface IChart extends IBox, ICommonProps {
+  title?: string
+  height?: number
+  numberOfTicks?: number
+  gridColor?: string
+  activeGridIndex?: number | undefined
+  activeGridColor?: string | undefined
+  values?: Array<ChartData>
+}
+
+export interface IDecorator extends ICommonProps {
+  radius?: number
+  fill?: string
+  data?: Array<number>
+  x?: (value: number) => number
+  y?: (value: number) => number
+}
+
+export interface IGrid extends GridProps<number>, ICommonProps {
+  activeIndex?: number
+  activeColor?: string
+  data?: Array<number>
+}
