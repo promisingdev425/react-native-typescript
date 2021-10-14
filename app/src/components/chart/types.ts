3a9ce562097ca775871b8a19aebd0efdf335ed34
smartdev425 <anthon.dev914@gmail.com>
2021-10-13 18:35:09 -0700

    add typedoc

--- app/src/components/chart/types.ts
+++ app/src/components/chart/types.ts
@@ -3,8 +3,17 @@ import { GridProps, AxisProps } from 'react-native-svg-charts'
 import { IBox } from '../core'
 
 interface ICommonProps {
+  /**
+   * Numeric value for for set visible space
+   */
   inset?: number
+  /**
+   * Stroke/border width of svg element
+   */
   strokeWidth?: number
+  /**
+   * Stroke/border color of svg element
+   */
   strokeColor?: string
 }
 
@@ -20,14 +29,41 @@ export enum ChartType {
 }
 
 export interface IChart extends IBox, ICommonProps {
+  /**
+   * The title of the chart
+   */
   title?: string
+  /**
+   * height can be a number or string to speicify chart's height
+   */
   height?: number
+  /**
+   * Grid count under the chart
+   */
   numberOfTicks?: number
+  /**
+   * Chart color
+   */
   fillColor?: string
+  /**
+   * Grid line color
+   */
   gridColor?: string
+  /**
+   * Active index to specify current cursor line
+   */
   activeGridIndex?: number | undefined
+  /**
+   * Active grid color
+   */
   activeGridColor?: string | undefined
+  /**
+   * Number array for yAxis values
+   */
   data?: Array<number>
+  /**
+   * Total chart data given by user - pair of x-label, y-values
+   */
   values?: Array<ChartData>
 }
 
@@ -40,9 +76,21 @@ export interface IDecorator extends ICommonProps {
 }
 
 export interface IGrid extends GridProps<number>, ICommonProps {
+  /**
+   * Active index to specify current cursor line
+   */
   activeIndex?: number
+  /**
+   * Active grid color
+   */
   activeColor?: string
+  /**
+   * One of Bar | Pie | Line
+   */
   type?: ChartType
+  /**
+   * Total chart data given by user - pair of x-label, y-values
+   */
   data?: Array<number>
 }
 
