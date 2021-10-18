da9424a048b15d42119890fdd9158b00f5655e24
smartdev425 <anthon.dev914@gmail.com>
2021-10-18 10:49:00 -0600

    lighthouse ci

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,9 @@
 module.exports = {
   ci: {
+    collect: {
+      startServerCommand: 'yarn web:mock',
+      url: ['http://localhost:19006']
+    },
     upload: {
       target: 'temporary-public-storage',
     },
