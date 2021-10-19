b37d692916ce276b94797199a97d06cfdf0a46b5
smartdev425 <anthon.dev914@gmail.com>
2021-10-19 09:46:04 -0600

    lighthouse ci

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,6 +1,8 @@
 module.exports = {
   ci: {
+    preset: 'lighthouse:recommended',
     collect: {
+      numberOfRuns: 2,
       startServerCommand: 'yarn workspace @apartmentsnapshot/snapshot-ui serve:web',
       url: ['http://localhost:5000']
     },
