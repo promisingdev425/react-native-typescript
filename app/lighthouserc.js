1c7cd9377ff442d6164dee068884d915a84b0936
smartdev425 <anthon.dev914@gmail.com>
2021-10-25 11:47:36 -0600

    lighthouse ci pr fixes

--- app/lighthouserc.js
+++ app/lighthouserc.js
@@ -7,13 +7,20 @@ module.exports = {
         'yarn workspace @apartmentsnapshot/snapshot-ui serve:web',
       url: ['http://localhost:5000'],
       settings: {
+        //throttlingMethod: 'devtools',
         throttling: {
-          cpuSlowdownMultiplier: 1,
+          cpuSlowdownMultiplier: 9,
         },
       },
     },
     upload: {
-      target: 'filesystem',
+      /*
+       * Only use 'temporary-public-storage' for troubleshooting on CircleCI
+       * You can't see the results within CircleCI.
+       * So this gives you a temporary url.
+       * Otherwise we should always use 'filesystem' to store results
+       */
+      target: 'temporary-public-storage',
       outputDir: '../reports/lighthouse',
     },
     assert: {
